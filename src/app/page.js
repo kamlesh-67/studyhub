c810695f45e03dc06cabe18bd5b06c8d7c3b737e
kamlesh_67 <kamlesh@retaillogis.com>
2025-01-13 15:05:31 +0530

    fisrt commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
-import Image from "next/image";
+import BookstorePage from "@/pages/bookstore/page";
+import ChapterDetails from "@/pages/DetailedPage/components/ChapterDetails";
+import DetailedPage from "@/pages/DetailedPage/page";
+import HomePage from "@/pages/home/page";
+import PaymentPage from "@/pages/payment/page";
+import Layout from "@/pages/Utils/Layout";
 
 export default function Home() {
   return (
     <>
-
-      
+      <Layout>
+        <HomePage />
+      </Layout>
     </>
   );
 }
